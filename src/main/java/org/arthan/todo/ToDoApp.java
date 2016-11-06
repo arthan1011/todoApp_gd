18beaba7ea039cb3dd6beb8e322015525c3551b1
arthan1011 <arthan1011@gmail.com>
2016-11-06 21:47:24 +0300

    apache lang dependency added

--- src/main/java/org/arthan/todo/ToDoApp.java
+++ src/main/java/org/arthan/todo/ToDoApp.java
@@ -1,5 +1,6 @@
 package org.arthan.todo;
 
+import org.apache.commons.lang3.CharUtils;
 import org.arthan.todo.util.CommandLineInput;
 import org.arthan.todo.util.CommandLineInputHandler;
 
@@ -16,8 +17,7 @@ public class ToDoApp {
         while (CommandLineInput.EXIT.getShortCmd() != command) {
             commandLineInputHandler.printOptions();
             String input = commandLineInputHandler.readInput();
-            char[] inputChars = input.length() == 1 ? input.toCharArray() : new char[]{DEFAULT_INPUT};
-            command = inputChars[0];
+            command = CharUtils.toChar(input, DEFAULT_INPUT);
             CommandLineInput commandLineInput = CommandLineInput.getCommandLineInputForInput(command);
             commandLineInputHandler.processInput(commandLineInput);
         }
