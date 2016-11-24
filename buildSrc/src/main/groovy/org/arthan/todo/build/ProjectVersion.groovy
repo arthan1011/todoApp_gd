de5037206339735de4e5768079fa7887a9c259d8
arthan1011 <arthan1011@gmail.com>
2016-11-24 20:43:40 +0300

    gitignore fixed

--- buildSrc/src/main/groovy/org/arthan/todo/build/ProjectVersion.groovy
+++ buildSrc/src/main/groovy/org/arthan/todo/build/ProjectVersion.groovy
@@ -4,4 +4,22 @@ package org.arthan.todo.build
  * Created by arthan on 24.11.2016. | Project todoApp_gd
  */
 class ProjectVersion {
+    Integer major;
+    Integer minor;
+    boolean release;
+
+    ProjectVersion(Integer major, Integer minor) {
+        this(major, minor, Boolean.FALSE)
+    }
+
+    ProjectVersion(Integer major, Integer minor, boolean release) {
+        this.major = major
+        this.minor = minor
+        this.release = release
+    }
+
+    @Override
+    public String toString() {
+        return "$major.$minor${release ? '' : '-SNAPSHOT'}";
+    }
 }
